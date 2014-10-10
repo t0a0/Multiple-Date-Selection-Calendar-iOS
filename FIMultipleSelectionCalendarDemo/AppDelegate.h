7ecdfac7e6a4754cf7adfab7841d0fcf5ab3624a
Igor F <jus1no1hing@gmail.com>
2014-10-10 18:00:38 +0400

    Initial Commit

---
+++ FIMultipleSelectionCalendarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FIMultipleSelectionCalendarDemo
+//
+//  Created by Igor on 10.10.14.
+//  Copyright (c) 2014 Fedotov Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FIMultipleSelectionCalendarDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FIMultipleSelectionCalendarDemo
+//
+//  Created by Igor on 10.10.14.
+//  Copyright (c) 2014 Fedotov Igor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
